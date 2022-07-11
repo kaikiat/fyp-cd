4a1bf5ce82dbc9f8dbdf1194edb9ffd691a9a4da
kaikiat <pohkaikiat98@gmail.com>
2022-07-11 15:53:01 +0800

    update to correct image

--- prometheus.sh
+++ prometheus.sh
@@ -7,14 +7,19 @@ kubectl port-forward deployment/prometheus-grafana 3000 -n prometheus
 # ArgoCD Helm
 # https://github.com/argoproj/argo-cd/discussions/8968
 # https://github.com/argoproj/argo-helm/tree/main/charts/argo-cd
+# https://ishanjainn.medium.com/monitor-argocd-applications-using-prometheus-and-alertmanager-f96d7f39b060
 
 ##### Not used #####
 helm repo add argo https://argoproj.github.io/argo-helm
-# Why cannot use argocd ? But argocd-release works
-helm install argocd-release argo/argo-cd --namespace argocd
-helm upgrade argocd-release argo/argo-cd -f prometheus_configuration/values.yaml -n argocd
-helm uninstall argocd-release -n argocd
+# helm install argocd-release argo/argo-cd --namespace argocd
+# helm upgrade argocd-release argo/argo-cd -f prometheus_configuration/values.yaml -n argocd
+# helm uninstall argocd-release -n argocd
 
 kubectl apply -f prometheus_configuration/service-monitor.yaml
+kubectl apply -f prometheus_configuration/prometheus-rule.yaml
 
-# https://ishanjainn.medium.com/monitor-argocd-applications-using-prometheus-and-alertmanager-f96d7f39b060
+##### Grafana API key #####
+# https://argo-cd.readthedocs.io/en/stable/operator-manual/notifications/services/grafana/
+export GRAFANA_KEY=eyJrIjoiNXVaTFRVNW5DU3YxM1BjOEYzd0NTOVJWMXZPU2dtdHYiLCJuIjoiR1JBRkFOQV9LRVkiLCJpZCI6MX0=
+kubectl apply -f prometheus_configuration/grafana-configmap.yaml
+kubectl patch app sgdecoding-online-scaled -n argocd -p '{"metadata": {"annotations": {"notifications.argoproj.io/subscribe.grafana-trigger.grafana":"argocd-grafana"}}}' --type merge
