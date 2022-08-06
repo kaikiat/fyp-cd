ed89a393db058fca6bee73563e9c63051bc73f02
kaikiat <pohkaikiat98@gmail.com>
2022-08-06 23:11:45 +0800

    modify service.yaml

--- ambassador.sh
+++ ambassador.sh
@@ -9,15 +9,6 @@
 # helm install edge-stack --namespace ambassador datawire/edge-stack && \
 # kubectl -n ambassador wait --for condition=available --timeout=90s deploy -lproduct=aes
 
-
-# https://www.getambassador.io/docs/edge-stack/latest/tutorials/quickstart-demo/
-# kubectl apply -f ambassador/mapping.yaml
-# kubectl apply -f ambassador/quote.yaml
-# export LB_ENDPOINT=$(kubectl -n ambassador get svc  edge-stack \
-#   -o "go-template={{range .status.loadBalancer.ingress}}{{or .ip .hostname}}{{end}}")
-# curl -Lki https://$LB_ENDPOINT/backend/
-
-
 ###################
 
 # Refer to https://www.getambassador.io/docs/argo/latest/howtos/configure-argo-rollouts/
@@ -25,4 +16,23 @@ kubectl apply -f https://app.getambassador.io/yaml/edge-stack/3.1.0/aes-crds.yam
 kubectl wait --timeout=90s --for=condition=available deployment emissary-apiext -n emissary-system
 kubectl apply -f https://app.getambassador.io/yaml/edge-stack/3.1.0/aes.yaml && \
 kubectl -n ambassador wait --for condition=available --timeout=90s deploy -lproduct=aes
+
+kubectl get deploy --namespace ambassador edge-stack -o jsonpath='{.spec.template.spec.containers[0].image}'
+
+# Adjust the api version of EdgeStack
+kubectl patch deployment -n argo-rollouts \
+    $(kubectl get -nargo-rollouts -l app.kubernetes.io/component=rollouts-controller deploy -o=jsonpath='{.items[].metadata.name}') \
+    -p '{"spec":{"template":{"spec":{"containers":[{"name":"argo-rollouts", "args":["--ambassador-api-version","getambassador.io/v3alpha1"]}]}}}}'
+
+sudo curl -fL https://metriton.datawire.io/downloads/darwin/edgectl \
+  -o /usr/local/bin/edgectl && \
+sudo chmod a+x /usr/local/bin/edgectl
+
+kubectl apply -f ./manifests --namespace ntuasr-production-google 
+
+# Click Existing, Edge Stack
+# Assuming your installation is in the ambassador namespace, run:
+kubectl create configmap --namespace ambassador edge-stack-agent-cloud-token --from-literal=CLOUD_CONNECT_TOKEN=NWZjYzQxNWYtNjU2MS00OGNlLTgwZmEtMTdhMTM0Y2VhNGIyOnJyblo3V3BqTnludXhIZTFWRWxNTXlYaFpzTHdCblJkQ0dKZg==
+
+
 
