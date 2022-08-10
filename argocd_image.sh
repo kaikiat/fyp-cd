faacea50f98991ad120e78dd0d760ce3711752d9
kaikiat <pohkaikiat98@gmail.com>
2022-08-10 23:08:37 +0800

    using failed image

--- argocd_image.sh
+++ argocd_image.sh
@@ -7,3 +7,4 @@ argocd account generate-token --account image-updater --id image-updater
 
 export API_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhcmdvY2QiLCJzdWIiOiJpbWFnZS11cGRhdGVyOmFwaUtleSIsIm5iZiI6MTY2MDE0MDE5OCwiaWF0IjoxNjYwMTQwMTk4LCJqdGkiOiJpbWFnZS11cGRhdGVyIn0.O4cJsgDpnldGSzho3yGG_1lXAHmZgP8CRwMwxme7R3c
 
+kubectl apply -f image_uploader/argocd-rbac-cm.yaml
