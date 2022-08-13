6c3ae62c4ca5dc9a0c9bcc4b67aa09502acdfee2
kaikiat <pohkaikiat98@gmail.com>
2022-08-13 19:47:54 +0800

    update secrets

--- sample/build.sh
+++ sample/build.sh
@@ -2,7 +2,6 @@
 export CR_PAT=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 
 
 docker build --tag python-docker --platform=linux/amd64 .
-# docker build --tag python-docker --platform=linux/arm64 .
 docker tag python-docker ghcr.io/kaikiat/python-docker:latest
 docker push ghcr.io/kaikiat/python-docker:latest
 
