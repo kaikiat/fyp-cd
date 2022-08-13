cf709597a4f908d0acc6bad780c48b1611adfc88
kaikiat <pohkaikiat98@gmail.com>
2022-08-13 23:42:16 +0800

    modify application.yaml

--- sample/application/app.py
+++ sample/application/app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Docker! Version latest vesion dasdad'
+    return 'Hello, Docker!'
--- sample/build.sh
+++ sample/build.sh
@@ -2,8 +2,10 @@
 export CR_PAT=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 
 
 docker build --tag python-docker --platform=linux/amd64 .
-docker tag python-docker ghcr.io/kaikiat/python-docker:latest
-docker push ghcr.io/kaikiat/python-docker:latest
+# docker tag python-docker ghcr.io/kaikiat/python-docker:latest
+# docker tag python-docker ghcr.io/kaikiat/python-docker:latest
+# docker push ghcr.io/kaikiat/python-docker:latest
+docker push ghcr.io/kaikiat/python-docker
 
 docker run --publish 8000:5000 python-docker
 
