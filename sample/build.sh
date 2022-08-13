db88ec59fba27edf33ffc3f9150fc0b8b776980e
kaikiat <pohkaikiat98@gmail.com>
2022-08-13 17:20:21 +0800

    update repo

--- sample/build.sh
+++ sample/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 export CR_PAT=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 
 
-docker build --tag python-docker --platform linux/amd64 .
+docker build --tag python-docker --platform=linux/amd64 .
+# docker build --tag python-docker --platform=linux/arm64 .
 docker tag python-docker ghcr.io/kaikiat/python-docker:latest
 docker push ghcr.io/kaikiat/python-docker:latest
+
+docker run --publish 8000:5000 python-docker
+
 
