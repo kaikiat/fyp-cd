92b7bca42c83ff442901720ab00be87371245b65
kaikiat <pohkaikiat98@gmail.com>
2022-08-23 23:42:13 +0800

    fix pullPolicy

--- argocd_image.sh
+++ argocd_image.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-image-updater/stable/manifests/install.yaml
 
-kubectl apply -f image_uploader/argocd-cm.yaml
-kubectl apply -f image_uploader/argocd-rbac-cm.yaml
+# kubectl apply -f image_uploader/argocd-cm.yaml
+# kubectl apply -f image_uploader/argocd-rbac-cm.yaml
 
-# API_KEY
-argocd account generate-token --account image-updater --id image-updater
-export API_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhcmdvY2QiLCJzdWIiOiJpbWFnZS11cGRhdGVyOmFwaUtleSIsIm5iZiI6MTY2MDM2MTk1OCwiaWF0IjoxNjYwMzYxOTU4LCJqdGkiOiJpbWFnZS11cGRhdGVyIn0.HEzxYOYjexkp48kuyOZwpTP8u-gQoCOFzCK9CSRAgf
-kubectl create secret generic argocd-image-updater-secret \
-  --from-literal argocd.token=$API_KEY --dry-run -o yaml |
-  kubectl -n argocd apply -f -
+kubectl patch configmap/argocd-cm --patch-file image_uploader/argocd-cm.yaml -n argocd
+# kubectl apply -f image_uploader/argocd-rbac-cm.yaml -n argocd
 
-  # Set log to debug level
+
+# API_KEY (seems useless ?)
+# argocd account generate-token --account image-updater --id image-updater
+# export API_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhcmdvY2QiLCJzdWIiOiJpbWFnZS11cGRhdGVyOmFwaUtleSIsIm5iZiI6MTY2MDM2MTk1OCwiaWF0IjoxNjYwMzYxOTU4LCJqdGkiOiJpbWFnZS11cGRhdGVyIn0.HEzxYOYjexkp48kuyOZwpTP8u-gQoCOFzCK9CSRAgf
+# kubectl create secret generic argocd-image-updater-secret \
+#   --from-literal argocd.token=$API_KEY --dry-run -o yaml |
+#   kubectl -n argocd apply -f -
+
+# Set log to debug level
 kubectl patch configmap/argocd-image-updater-config \
   -n argocd \
   --type merge \
-  -p '{"metadata":{"data":{"log.level":"debug"}}}'
+  -p '{"data":{"log.level":"debug"}}'
+  # -p '{"metadata":{"data":{"log.level":"debug"}}}'
 
 # GITHUB TOKEN
 kubectl create secret generic git-creds \
@@ -24,6 +29,7 @@ kubectl create secret generic git-creds \
   -n argocd
 
 kubectl -n argocd rollout restart deployment argocd-image-updater
+kubectl logs -n argocd -l app.kubernetes.io/name=argocd-image-updater -f
 
 
 
