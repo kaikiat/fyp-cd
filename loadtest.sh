53c96521d82f356135238b7646767e59ec6a610f
kaikiat <pohkaikiat98@gmail.com>
2022-09-25 21:06:18 +0800

    init blue green

--- loadtest.sh
+++ loadtest.sh
@@ -1,5 +1,5 @@
 
-printf $"Please select an option \n1) Test canary \n2) Test original service (Blue) \n3) Test preview service (Green) \n \n Please open a new command prompt for 2 & 3"
+printf $"Please select an option \n1) Test canary \n2) Test original service (Blue) \n3) Test preview service (Green) \n \n Please open a new command prompt for 2 & 3\n"
 read input
 echo $input
 if [ $input == "1" ]; then
--- suite/blue.py
+++ suite/blue.py
@@ -1,6 +1,7 @@
 '''
     Blue represents the older version
 '''
+import os
 import logging
 import subprocess
 import time
@@ -13,19 +14,12 @@ logger.setLevel(logging.INFO)
 def main():
     cmd = r"kubectl get svc sgdecoding-online-scaled-master -n ntuasr-production-google --output jsonpath='{.status.loadBalancer.ingress[0].ip}'"
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell = True)
-    output = process.communicate()
-    ip_address = output.decode('utf-8')
-    logger.info(f'Ip Address : {ip_address}')
-
-
-    cmd_preview = r"kubectl get svc sgdecoding-online-scaled-master -n ntuasr-production-google --output jsonpath='{.status.loadBalancer.ingress[0].ip}'"
-    process = subprocess.Popen(cmd_preview, stdout=subprocess.PIPE, shell = True)
     output, error = process.communicate()
     ip_address = output.decode('utf-8')
     logger.info(f'Ip Address : {ip_address}')
     
     min = 60
-    duration = min * 5
+    duration = min * 2 # must be 2 minutes due to configuration
     time.sleep(min * 3)
     end = int(time.time()) + duration
     
@@ -34,6 +28,7 @@ def main():
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell = True)
         output, error = process.communicate()
         logger.info(output)
+        time.sleep(10)
 
 if __name__ == "__main__":
     main()
