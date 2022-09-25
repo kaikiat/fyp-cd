9cb6d732eaa1d800a69fcfc7cfa6e5795e682160
kaikiat <pohkaikiat98@gmail.com>
2022-09-25 17:11:33 +0800

    use sample app

--- .sample/build.sh
+++ .sample/build.sh
@@ -2,10 +2,10 @@
 export CR_PAT=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
 
 
-export VERSION=0.0.2
-docker build --tag python-docker:0.0.3 --platform=linux/amd64 .
-docker tag python-docker:0.0.3 ghcr.io/kaikiat/python-docker:0.0.3
-docker push ghcr.io/kaikiat/python-docker:0.0.3
+export VERSION=0.0.5
+docker build --tag python-docker:0.0.5 --platform=linux/amd64 .
+docker tag python-docker:0.0.5 ghcr.io/kaikiat/python-docker:0.0.5
+docker push ghcr.io/kaikiat/python-docker:0.0.5
 
 
 echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin
